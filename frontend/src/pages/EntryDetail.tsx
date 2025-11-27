c22ba9cc4048c4ba8ee3a193181c683ffb9dbc78
Benja <benj.vilches@duocuc.cl>
2025-11-27 00:49:50 -0300

    Fix: Corregir estructura de divs en EntryDetail.tsx - agregar cierre faltante de lg:col-span-2

--- frontend/src/pages/EntryDetail.tsx
+++ frontend/src/pages/EntryDetail.tsx
@@ -388,6 +388,7 @@ export default function EntryDetail() {
                   })}
                 </div>
               )}
+            </div>
           </div>
 
           {/* Información del Vehículo */}
